b62d0007b64292e4805b8c65821f4f77a9353971
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-03-28 18:37:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# py3oauth2
+This is a OAuth 2.0 library for Python 3 developed at [GehirnInc](http://www.gehirn.co.jp/).
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='py3oauth2',
+    version='0.0.1',
+    author='Kohei YOSHIDA',
+    author_email='kohei@yosida95.com',
+    packages=['py3oauth2'],
+    scripts=[],
+    url='https://github.com/GehirnInc/py3oauth2',
+    license='LICENSE.md',
+    description='OAuth 2.0 library for Python 3.',
+    long_description=open('README.md').read(),
+    install_requires=[])
