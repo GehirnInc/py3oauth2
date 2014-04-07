b307928511331290e3d0dd8e27e4dd4400f14564
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-07 21:18:46 +0900

    reimpl

--- py3oauth2/provider/provider.py
+++ py3oauth2/provider/provider.py
@@ -9,47 +9,14 @@ class AuthorizationProvider:
     def __init__(self, store):
         self.store = store
 
+    def validate_client(self, client):
+        raise NotImplementedError
+
     def generate_authorization_code(self):
         return utils.generate_random_string(
             self.store.get_authorization_code_length(),
             utils.RSFlag.LOWER | utils.RSFlag.UPPER | utils.RSFlag.DIGITS
         )
-
-    def issue_authorization_code(self, owner, request):
-        try:
-            client = self.store.get_client(request.client_id)
-            if client is None:
-                raise ValueError('get_client() returned None')
-        except ValueError:
-            resp = request.err_response(request)
-            resp.error = 'unauthorized_client'
-            return resp
-        except:
-            resp = request.err_response(request)
-            resp.error = 'server_error'
-            return resp
-        else:
-            if not hasattr(request, 'response_type')\
-                    or request.response_type is None:
-                resp = request.err_response(request)
-                resp.error = 'invalid_request'
-                return resp
-            elif request.response_type != 'code':
-                resp = request.err_response(request)
-                resp.error = 'unsupported_response_type'
-                return resp
-
-            code = self.generate_authorization_code()
-            try:
-                codeobj = self.store.persist_authorization_code(
-                    client, owner, code, request.scope
-                )
-            except DenyAuthentication:
-                resp = request.err_response(request)
-                resp.error = 'access_denied'
-                return resp
-            else:
-                return request.response.from_request(request, codeobj)
 
     def generate_access_token(self):
         return utils.generate_random_string(
@@ -57,11 +24,41 @@ class AuthorizationProvider:
             utils.RSFlag.LOWER | utils.RSFlag.UPPER | utils.RSFlag.DIGITS
         )
 
-    def issue_access_token(self, owner, request):
+    def handle_request(self, request, owner=None):
+        if hasattr(request, 'response_type'):
+            if request.response_type == 'code':
+                # authorization code grant
+                return self._handle_authcode_code(request, owner)
+            elif request.response_type == 'token':
+                # implicit grant
+                return self._handle_implicit_access_token(request, owner)
+            else:
+                resp = request.err_response(request)
+                resp.error = 'unsupported_response_type'
+                return resp
+        elif hasattr(request, 'grant_type'):
+            if request.grant_type == 'authorization_code':
+                return self._handle_authcode_token(request)
+            elif request.grant_type == 'refresh_token':
+                return self._handle_refresh_token(request)
+            else:
+                resp = request.err_response(request)
+                resp.error = 'unsupported_grant_type'
+                return resp
+        else:
+            resp = request.err_response(request)
+            resp.error = 'invalid_request'
+            return resp
+
+    def _handle_authcode_code(self, request, owner):
         try:
             client = self.store.get_client(request.client_id)
-            if client is None:
-                raise ValueError('get_client() returned None')
+            if client is None or not self.validate_client(client):
+                raise ValueError()
+
+            code = self.store.persist_authorization_code(
+                client, owner,
+                self.generate_authorization_code(), request.scope)
         except ValueError:
             resp = request.err_response(request)
             resp.error = 'unauthorized_client'
@@ -71,47 +68,67 @@ class AuthorizationProvider:
             resp.error = 'server_error'
             return resp
         else:
-            if 'grant_type' in request:
-                try:
-                    if request.grant_type == 'authorization_code':
-                        # authorization code grant
-                        self.store.discard_authorization_code(client,
-                                                              request.code)
-                    elif request.grant_type == 'refresh_token':
-                        # refresh token
-                        self.store.discard_refresh_token(client,
-                                                         request.refresh_token)
-                    else:
-                        resp = request.err_response(request)
-                        resp.error = 'unsupported_grant_type'
-                        return resp
-                except AttributeError:
-                    err = request.err_response(request)
-                    err.error = 'invalid_request'
-                    return err
-                except ValueError:
-                    err = request.err_response(request)
-                    err.error = 'access_denied'
-                    return err
-            elif 'response' in request:
-                # implicit grant
-                if request.response != 'token':
-                    resp = request.err_response(request)
-                    resp.error = 'unsupported_response'
-                    return resp
-            else:
-                resp = request.err_response(request)
-                resp.error = 'invalid_request'
-                return
+            return request.response.from_request(request, code)
 
-            token = self.generate_access_token()
-            expires_in = client.get_access_token_expires_in(owner)
+    def _handle_authcode_token(self, request):
+        try:
+            authcode = self.store.get_authorization_code(request.code)
+            if authcode.is_used:
+                raise ValueError()
+
             try:
-                tokenobj = self.store.persist_access_token(
-                    client, owner, token, expires_in)
-            except DenyAuthentication:
+                client = self.store.get_client(request.client_id)
+                if not self.validate_client(client)\
+                        or client.get_id() != authcode.get_client().get_id():
+                    raise ValueError()
+            except ValueError:
                 resp = request.err_response(request)
-                resp.error = 'access_denied'
+                resp.error = 'unauthorized_client'
                 return resp
-            else:
-                return request.response.from_request(request, tokenobj)
+
+            token = self.store.persist_access_token(
+                authcode.get_client(), authcode.get_owner(),
+                self.generate_access_token(), authcode.get_scope())
+        except ValueError:
+            resp = request.err_response(request)
+            resp.error = 'access_denied'
+            return resp
+        else:
+            return request.response.from_request(request, token)
+
+    def _handle_implicit_access_token(self, request, owner):
+        try:
+            client = self.store.get_client(request.client_id)
+            if not self.validate_client(client):
+                raise ValueError()
+
+            token = self.store.persist_access_token(
+                client, owner, self.generate_access_token(), request.scope)
+        except ValueError:
+            resp = request.err_response(request)
+            resp.error = 'unauthorized_client'
+            return resp
+        except:
+            resp = request.err_response(request)
+            resp.error = 'server_error'
+            return resp
+        else:
+            return request.response.from_request(request, token)
+
+    def _handle_refresh_token(self, request):
+        try:
+            previous = self.store.from_refresh_token(request.refresh_token)
+            self.store.discard_access_token(previous)
+            token = self.store.persist_access_token(
+                previous.get_client(), previous.get_owner(),
+                self.generate_access_token())
+        except ValueError:
+            resp = request.err_response(request)
+            resp.error = 'access_denied'
+            return resp
+        except:
+            resp = request.err_response(request)
+            resp.error = 'server_error'
+            return resp
+        else:
+            return request.response.from_request(request, token)
