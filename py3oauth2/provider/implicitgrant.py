8fc4ae1a93370156076218f297fdf98db3a22b1b
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-07 21:16:45 +0900

    impl Response#from_request

--- py3oauth2/provider/implicitgrant.py
+++ py3oauth2/provider/implicitgrant.py
@@ -15,6 +15,21 @@ class Response(message.Response):
     scope = message.Parameter(str)
     state = message.Parameter(str, is_state_required)
 
+    @classmethod
+    def from_request(cls, request, token):
+        D = {
+            'access_token': token.get_token(),
+            'token_type': token.get_type(),
+            'expires_in': token.get_expires_in(),
+        }
+        if hasattr(request, 'scope') and request.scope != token.get_scope():
+            D['scope'] = token.get_scope()
+
+        if hasattr(request, 'state') and request.state is not None:
+            D['state'] = request.state
+
+        return cls.from_dict(request, D)
+
 
 class ErrorResponse(message.Response):
 
