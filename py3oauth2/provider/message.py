aea8c5f93a09d86594c0b2ba96bc3e7207663139
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-08 18:33:21 +0900

    reimpl Message and impl Constant

--- py3oauth2/provider/message.py
+++ py3oauth2/provider/message.py
@@ -5,50 +5,31 @@ import json
 from .exceptions import ValidationError
 
 
-class Value:
-
-    def __init__(self, param, name, value):
-        assert isinstance(param, Parameter)
-
-        self.param = param
-        self.name = name
-        self.__set__(self, value)
-
-    def __get__(self, inst, owner):
-        return self._value
-
-    def __set__(self, inst, value):
-        if self.param.type is None\
-                or isinstance(value, (self.param.type, type(None))):
-            self._value = value
-            return
-
-        raise ValidationError('%s must be an instance of %r' % (
-            self.name, self.param.type,
-        ))
-
-
 class Parameter:
 
-    def __init__(self, type, required=False, recommended=False, default=None):
-
+    def __init__(self, type, required=False, recommended=False,
+                 default=None, editable=True):
         self.type = type
         self.required = required
         self.recommended = recommended
         self.default = default
+        self.editable = editable
 
         self._check_type(self, 'value of argument default', self.default)
 
-    def get_default(self):
-        return self.default
-
-    def new(self, name, value):
-        return Value(self, name, value)
+    def new(self, name):
+        if self.editable:
+            return Variable(self, name)
+        else:
+            return Constant(self, name)
 
     def validate(self, owner, name, value, required=True):
-        return all((
-            self._check_required(owner, name,  value),
-            self._check_type(owner, name, value)))
+        chain = []
+        if required:
+            chain.append(lambda: self._check_required(owner, name,  value))
+        chain.append(lambda: self._check_type(owner, name, value))
+
+        return all(f() for f in chain)
 
     def _check_required(self, owner, name, value):
         required =\
@@ -68,30 +49,71 @@ class Parameter:
         return True
 
 
+class Variable:
+
+    def __init__(self, param, name):
+        assert isinstance(param, Parameter)
+
+        self.param = param
+        self.name = name
+
+    def __get__(self, inst, owner):
+        if inst is None:
+            return self.param
+
+        assert isinstance(inst, Message)
+
+        if self.name not in inst.__dict__:
+            self.__set__(inst, self.param.default)
+
+        return inst.__dict__[self.name]
+
+    def __set__(self, inst, value):
+        assert isinstance(inst, Message)
+
+        if self.param.validate(inst, self.name, value, False):
+            inst.__dict__[self.name] = value
+
+    def __delete__(self, instance):
+        if self.name in instance.__dict__:
+            del instance.__dict__[self.name]
+
+
+class Constant(Variable):
+
+    def __get__(self, inst, owner):
+        if inst is None:
+            return self.param
+
+        assert isinstance(inst, Message)
+        return self.param.default
+
+    def __set__(self, inst, value):
+        assert isinstance(inst, Message)
+
+        raise ValidationError('{0!s} is not editable'.format(self.name))
+
+    def __delete__(self, instance):
+        raise ValidationError('{0!s} is not editable'.format(self.name))
+
+
 class MessageMeta(type):
 
     def __new__(cls, name, bases, namespace):
-        namespace['__msg_params__'] = dict(
-            (k, v) for k, v in namespace.items() if isinstance(v, Parameter)
-        )
+        params = {}
+        for k, v in namespace.items():
+            if not isinstance(v, Parameter):
+                continue
+
+            params[k] = v
+            namespace[k] = v.new(k)
+        else:
+            namespace['__msg_params__'] = params
+
         return super(MessageMeta, cls).__new__(cls, name, bases, namespace)
 
 
 class Message(dict, metaclass=MessageMeta):
-
-    def __new__(cls, *args, **kwargs):
-        inst = super(Message, cls).__new__(cls, *args, **kwargs)
-        for name, value in cls.__msg_params__.items():
-            setattr(inst, name, value.new(name, value.get_default()))
-
-        return inst
-
-    def __getattribute__(self, name):
-        value = super(Message, self).__getattribute__(name)
-        if hasattr(value, '__get__'):
-            return value.__get__(None, self)
-
-        return value
 
     def __str__(self):
         return self._to_dict().__str__()
@@ -119,20 +141,28 @@ class Message(dict, metaclass=MessageMeta):
 
         return True
 
+    def _from_dict(self, D):
+        for k, v in D.items():
+            if k in self.__msg_params__:
+                setattr(self, k, v)
+            else:
+                self[k] = v
+        else:
+            self.validate()
+
     @classmethod
     def from_dict(cls, D):
         inst = cls()
-        for k, v in D.items():
-            if hasattr(inst, k):
-                setattr(inst, k, v)
-            else:
-                inst[k] = v
-
-        inst.validate()
+        inst._from_dict(D)
         return inst
 
 
-Request = type('Request', (Message, ), {})
+class Request(Message):
+    response = None
+    err_response = None
+
+    def handle(self, *args, **kwargs):
+        raise NotImplementedError
 
 
 class Response(Message):
@@ -144,13 +174,7 @@ class Response(Message):
     @classmethod
     def from_dict(cls, request, D):
         inst = cls(request)
-        for k, v in D.items():
-            if hasattr(inst, k):
-                setattr(inst, k, v)
-            else:
-                inst[k] = v
-
-        inst.validate()
+        inst._from_dict(D)
         return inst
 
 
---
+++ py3oauth2/provider/tests/test_message.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+
+import json
+import unittest
+
+
+class TestParameter(unittest.TestCase):
+
+    @property
+    def target_class(self):
+        from ..message import Parameter
+        return Parameter
+
+    def test_new_atom(self):
+        from ..message import Constant
+        inst = self.target_class(str, editable=False)
+        self.assertIsInstance(inst.new('name'), Constant)
+
+    def test_new_value(self):
+        from ..message import Variable
+        inst = self.target_class(str)
+        self.assertIsInstance(inst.new('name'), Variable)
+
+    def test_validate(self):
+        from ..exceptions import ValidationError
+        inst = self.target_class(str, required=True)
+
+        self.assertTrue(inst.validate(None, 'value', 'value'))
+        with self.assertRaises(ValidationError):
+            self.assertTrue(inst.validate(None, 'value', None))
+        self.assertTrue(
+            inst.validate(None, 'value', 'value', required=False))
+
+        with self.assertRaises(ValidationError):
+            inst.validate(None, 'value', 123)
+
+        with self.assertRaises(ValidationError):
+            inst.validate(None, 'value', 123, required=False)
+
+    def test_validate_required_func(self):
+        from ..exceptions import ValidationError
+        inst = self.target_class(
+            str, required=lambda owner: hasattr(owner, 'name'))
+
+        self.assertTrue(inst.validate(None, 'value', None))
+        with self.assertRaises(ValidationError):
+            inst.validate(type('Dummy', (object, ), {'name': 'value'})(),
+                          'value', None)
+        self.assertTrue(inst.validate(
+            type('Dummy', (object, ), {'name': 'value'})(), 'value', 'value'))
+
+
+class TestVariable(unittest.TestCase):
+
+    def setUp(self):
+        from ..message import (
+            Message,
+            Parameter,
+        )
+        self.foo = Parameter(str)
+        self.bar = Parameter(str, default='hoge')
+        self.msg = type('cls', (Message, ), {
+            'foo': self.foo,
+            'bar': self.bar,
+        })
+
+    def test_it(self):
+        self.assertEqual(self.msg.foo, self.foo)
+        self.assertEqual(self.msg.bar, self.bar)
+
+        inst = self.msg()
+        self.assertIsNone(inst.foo)
+        self.assertEqual(inst.bar, 'hoge')
+
+        inst.foo = inst.bar = 'value'
+        self.assertEqual(inst.foo, 'value')
+        self.assertEqual(inst.bar, 'value')
+
+        from ..exceptions import ValidationError
+        with self.assertRaises(ValidationError):
+            inst.foo = 123
+
+        del inst.foo
+        self.assertIsNone(inst.foo)
+
+        del inst.bar
+        self.assertEqual(inst.bar, 'hoge')
+
+
+class TestConstant(unittest.TestCase):
+
+    def setUp(self):
+        from ..message import (
+            Message,
+            Parameter,
+        )
+        self.foo = Parameter(str, default='foo', editable=False)
+        self.msg = type('cls', (Message, ), {
+            'foo': self.foo,
+        })
+
+    def test_it(self):
+        self.assertEqual(self.msg.foo, self.foo)
+
+        inst = self.msg()
+        self.assertEqual(inst.foo, 'foo')
+
+        from ..exceptions import ValidationError
+        with self.assertRaises(ValidationError):
+            inst.foo = 'hoge'
+
+        with self.assertRaises(ValidationError):
+            del inst.foo
+
+
+class TestMessageMeta(unittest.TestCase):
+
+    def test_it(self):
+        from ..message import (
+            Constant,
+            Parameter,
+            MessageMeta,
+            Variable,
+        )
+
+        foo = Parameter(str)
+        bar = Parameter(str, editable=False)
+        cls = MessageMeta('cls', (), {
+            'foo': foo,
+            'bar': bar,
+        })
+
+        self.assertTrue(hasattr(cls, '__msg_params__'))
+        self.assertEqual(cls.__msg_params__, {
+            'foo': foo,
+            'bar': bar,
+        })
+
+        self.assertIsInstance(cls.__dict__['foo'], Variable)
+        self.assertIsInstance(cls.__dict__['bar'], Constant)
+
+
+class TestMessage(unittest.TestCase):
+
+    def setUp(self):
+        from ..message import (
+            Message,
+            Parameter,
+        )
+        self.msg = type('cls', (Message, ), {
+            'foo': Parameter(str, editable=False),
+        })
+
+    def test_it(self):
+        inst = self.msg()
+        self.assertEqual(inst._to_dict(), {'foo': None})
+
+        inst['bar'] = 'value'
+        self.assertEqual(inst._to_dict(), {'foo': None, 'bar': 'value'})
+
+        self.assertEqual(eval(str(inst)), inst._to_dict())
+        self.assertEqual(eval(repr(inst)), inst._to_dict())
+
+        self.assertEqual(json.loads(inst.to_json()), {'bar': 'value'})
+
+        inst = self.msg.from_dict({
+            'bar': 'value',
+        })
+        self.assertIsNone(inst.foo)
+        self.assertEqual(inst['bar'], 'value')
+
+        from ..exceptions import ValidationError
+        with self.assertRaises(ValidationError):
+            inst = self.msg.from_dict({
+                'foo': 'value',
+            })
