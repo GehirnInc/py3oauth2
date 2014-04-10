aab2e81a36cc45360e4e69753280a8c15adf365b
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-10 14:10:21 +0900

    update setup.py and use tox

--- setup.py
+++ setup.py
@@ -1,14 +1,43 @@
-from distutils.core import setup
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+requires = []
+tests_require=[
+    'nose',
+    'coverage'
+]
+
+try:
+    import enum
+except ImportError:
+    requires.append('enum34')
+
+
+here = os.path.dirname(__file__)
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+
+
+readme = _read("README.md")
+license = _read("LICENSE.md")
 
 setup(
     name='py3oauth2',
     version='0.0.1',
+    test_suite='py3oauth2',
     author='Kohei YOSHIDA',
     author_email='kohei.yoshida@gehirn.co.jp',
-    packages=['py3oauth2'],
-    scripts=[],
-    url='https://github.com/GehirnInc/py3oauth2',
-    license='LICENSE.md',
     description='OAuth 2.0 library for Python 3.',
-    long_description=open('README.md').read(),
-    install_requires=[])
+    long_description=readme,
+    license=license,
+    url='https://github.com/GehirnInc/py3oauth2',
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=tests_require,
+)
