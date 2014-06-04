ecbad8e46f434ee3b3f536ebd98732689c48bed1
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-06-05 03:28:59 +0900

    remove examples package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 here = os.path.dirname(__file__)
 requires = []
@@ -36,7 +36,7 @@ license = _read("LICENSE.rst")
 
 setup(
     name='py3oauth2',
-    version='0.3.1',
+    version='0.3.2',
     test_suite='py3oauth2',
     author='Kohei YOSHIDA',
     author_email='kohei.yoshida@gehirn.co.jp',
@@ -44,7 +44,7 @@ setup(
     long_description=readme,
     license=license,
     url='https://github.com/GehirnInc/py3oauth2',
-    packages=find_packages(),
+    packages=['py3oauth2'],
     install_requires=requires,
     tests_require=tests_require,
     classifiers=[
